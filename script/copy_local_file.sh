ce259956a14ff085b3be0e664d5f2a46f003f6f8
yennanliu <f339339@gmail.com>
2019-08-28 08:12:17 +0800

    add func check if local dataset already exist, if not, git clone it

--- script/copy_local_file.sh
+++ script/copy_local_file.sh
@@ -3,6 +3,18 @@
 # SCRIPT COPY TOY DATA WITHIN LOCAL FILE SYSTEM   
 #################################################################
 
+check_file_exist(){
+
+if [[ -d $HOME/analysis ]]; 
+    then 
+        echo ">>>> file (dataset) already exist"
+    else 
+        echo ">>>> file (dataset) not exist, download them now..."
+        #cd ~ && git clone https://github.com/yennanliu/analysis.git
+        echo ">>>> download OK!"
+fi
+}
+
 copy_txt_file(){
 
 for txtfile in $(ls $HOME/analysis/SPARK_/*.txt);
@@ -11,7 +23,7 @@ for txtfile in $(ls $HOME/analysis/SPARK_/*.txt);
         cp $txtfile data/
     done 
 }
-copy_txt_file(){
+copy_csv_file(){
 
 for csvfile in $(ls $HOME/analysis/SPARK_/*.csv);
     do
@@ -20,5 +32,6 @@ for csvfile in $(ls $HOME/analysis/SPARK_/*.csv);
     done 
 } 
 
+check_file_exist
 copy_txt_file
-copy_txt_file
+copy_csv_file
