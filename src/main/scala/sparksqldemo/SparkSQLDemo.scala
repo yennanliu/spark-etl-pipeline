f22788c32e7ac62699ccb3590badf6e947e4e812
yennanliu <f339339@gmail.com>
2019-10-02 15:56:10 +0800

    add sparksqldemo and relative example.json

---
+++ src/main/scala/sparksqldemo/SparkSQLDemo.scala
@@ -0,0 +1,27 @@
+package sparksqldemo
+
+import org.apache.spark.sql.SaveMode
+// $example on:init_session$
+import org.apache.spark.sql.SparkSession
+
+object SparkSQLDemo {
+  def main(args: Array[String]) {
+
+    val spark = SparkSession
+      .builder
+      .appName("Spark sql demo")
+      .config("spark.master", "local")
+      .getOrCreate()
+
+    import spark.implicits._
+
+    // load the txt file 
+    val filePath = "data/example.json" 
+    val df = spark.read.json(filePath)
+    // show the df
+    df.show()
+    // show the df schema 
+    df.printSchema()
+  }
+
+}
