998e2a1f846003eff1d7470cdbf6c1cd0f181219
yennanliu <f339339@gmail.com>
2019-10-03 15:32:12 +0800

    add spark-stream hello world

---
+++ src/main/scala/StreamHelloworld/PrintTwitterStream.scala
@@ -0,0 +1,25 @@
+package StreamHelloworld
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.streaming.twitter.TwitterUtils
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import util.Twitter
+
+object PrintTwitterStream {
+  def main(args: Array[String]): Unit = {
+    Twitter.initialize()
+
+    val ssc = new StreamingContext("local[*]", "PrintTwitterStream", Seconds(1))
+
+    Logger.getRootLogger.setLevel(Level.ERROR)
+
+    val tweets = TwitterUtils.createStream(ssc, None)
+
+    println(">>>>> start stream...")
+
+    tweets.map(status => status.getText).print
+
+    ssc.start
+    ssc.awaitTermination()
+  }
+}
