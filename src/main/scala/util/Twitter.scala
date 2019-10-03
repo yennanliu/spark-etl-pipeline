776745f694ec062331b8e468d0e502d9f27bc2d6
yennanliu <f339339@gmail.com>
2019-10-03 15:32:39 +0800

    modify util Twitter.scala to load twitter creds via config file

--- src/main/scala/util/Twitter.scala
+++ src/main/scala/util/Twitter.scala
@@ -1,10 +1,23 @@
 package util
 
+import java.io.File
+import com.typesafe.config.ConfigFactory
+
+// object Twitter {
+//     def initialize(): Unit = {
+//         System.setProperty("twitter4j.oauth.consumerKey", sys.env("TWITTER_COSUMER_KEY"))
+//         System.setProperty("twitter4j.oauth.consumerSecret", sys.env("TWITTER_COSUMER_SECRET"))
+//         System.setProperty("twitter4j.oauth.accessToken", sys.env("TWITTER_TOKEN"))
+//         System.setProperty("twitter4j.oauth.accessTokenSecret", sys.env("TWITTER_TOKEN_SECRET"))
+//     }
+// }
+
 object Twitter {
     def initialize(): Unit = {
-        System.setProperty("twitter4j.oauth.consumerKey", sys.env("TWITTER_COSUMER_KEY"))
-        System.setProperty("twitter4j.oauth.consumerSecret", sys.env("TWITTER_COSUMER_SECRET"))
-        System.setProperty("twitter4j.oauth.accessToken", sys.env("TWITTER_TOKEN"))
-        System.setProperty("twitter4j.oauth.accessTokenSecret", sys.env("TWITTER_TOKEN_SECRET"))
+        val config = ConfigFactory.parseFile(new File("config/twitter.config"))
+        System.setProperty("twitter4j.oauth.consumerKey", config.getString("TWITTER_COSUMER_KEY"))
+        System.setProperty("twitter4j.oauth.consumerSecret", config.getString("TWITTER_COSUMER_SECRET"))
+        System.setProperty("twitter4j.oauth.accessToken", config.getString("TWITTER_TOKEN"))
+        System.setProperty("twitter4j.oauth.accessTokenSecret", config.getString("TWITTER_TOKEN_SECRET"))
     }
 }
