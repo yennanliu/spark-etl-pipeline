bf8449414c47d16665453736a0f1a05abaed4741
yennanliu <f339339@gmail.com>
2019-10-04 14:45:19 +0800

    add stream WindowOperation.scala demo and fix layout (SaveTweets.scala)

--- src/main/scala/StreamBasics/WindowOperation.scala
+++ src/main/scala/StreamBasics/WindowOperation.scala
@@ -5,24 +5,25 @@ import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.streaming.twitter.TwitterUtils
 import util.Twitter
 
-object SaveTweets {
+object WindowOperation {
     def main(args: Array[String]): Unit = {
         Twitter.initialize()
 
-        val ssc = new StreamingContext("local[*]", "SaveTweets", Seconds(1))
+        val ssc = new StreamingContext("local[*]", "WindowOperation", Seconds(1))
 
         Logger.getRootLogger.setLevel(Level.ERROR)
 
         val tweets = TwitterUtils.createStream(ssc, None)
 
         println(">>>>> start stream...")
-        println(">>>>> save stream...")
-        
-        tweets.repartition(1)
-            .saveAsTextFiles("output/SaveTweets/batch", "json")
+        println(">>>>> run WindowOperation...")
+
+        tweets.filter(_.getHashtagEntities.length > 0)
+            .map(_.getHashtagEntities.head.getText)
+            .window(Seconds(5))
+            .print
 
         ssc.start
         ssc.awaitTermination()
     }
-
 }
