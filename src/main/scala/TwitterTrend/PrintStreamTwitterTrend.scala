6155a8c6c904149cafb3c4b499941d2a115a7f60
yennanliu <f339339@gmail.com>
2019-10-20 14:48:58 +0800

    fix code layout

--- src/main/scala/TwitterTrend/PrintStreamTwitterTrend.scala
+++ src/main/scala/TwitterTrend/PrintStreamTwitterTrend.scala
@@ -27,15 +27,9 @@ object PrintStreamTwitterTrend {
                 val spark = SparkSession.builder.config(rdd.sparkContext.getConf).getOrCreate()
                 import spark.implicits._
 
-                // Convert RDD to DataFrame
                 val df = rdd.toDF("username","place","time", "hasg_tag")
-
-                // Create a temporary view
                 df.createOrReplaceTempView("twitter")
-
                 spark.sql("select * from twitter").show()
-
-                //spark.sql("select text_length, count(*) from twitter group by 1 order by 2 desc").show()
             }
 
     ssc.start
