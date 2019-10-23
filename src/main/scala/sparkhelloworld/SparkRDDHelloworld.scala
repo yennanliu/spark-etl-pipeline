c33b2de6d7a5424e38f70ed66af3897606cb4e92
yennanliu <f339339@gmail.com>
2019-10-23 17:12:25 +0800

    update SparkRDDHelloworld.scala with toy data

--- src/main/scala/sparkhelloworld/SparkRDDHelloworld.scala
+++ src/main/scala/sparkhelloworld/SparkRDDHelloworld.scala
@@ -1,24 +1,28 @@
 package sparkhelloworld 
 
-import org.apache.spark.SparkConf
+import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.SparkSession 
 
 object SparkRDDHelloworld{
 
-	def main(args: Array[String]){
+    def main(args: Array[String]){
 
-		var conf = new SparkConf().setAppName("SparkRDDHelloworld")
-								  .setMaster("spark://172.1.1.1.:7007")
-		var spark : SparkSession = SparkSession.builder
-											   .config(conf)
-											   .config("spark.master", "local")
-											   .getOrCreate()
-		print (">>>>>>>>>>")
-		var rdd = spark.sparkContext.parallelize(Array(1 to 30))
-		rdd.collect()
-		rdd.count()
-		spark.stop()
-		print ("SparkRDDHelloworld")
-		print (">>>>>>>>>>")
-	}
+        val conf = new SparkConf().setMaster("local[*]").setAppName("SparkRDDHelloworld")
+
+        // var spark : SparkSession = SparkSession.builder
+        //                                     .config(conf)
+        //                                     .config("spark.master", "local")
+        //                                     .getOrCreate()
+        var sc = new SparkContext(conf)
+        sc.setLogLevel("ERROR")
+        print (">>>>>>>>>>")
+        var filePath = "data/pic_data.txt"
+        val lines = sc.textFile(filePath)
+        val words = lines.flatMap(line => line.split(" "))
+        val counts = words.map(word => (word, 1)).reduceByKey { case (x, y) => x + y }
+        counts.collect().foreach(println)
+        sc.stop()
+        print ("SparkRDDHelloworld")
+        print (">>>>>>>>>>")
 }
+ } 
