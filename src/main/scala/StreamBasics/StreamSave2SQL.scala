86b259fcf90406ffcf2da5323022e1d3f389e063
yennanliu <f339339@gmail.com>
2019-10-23 15:05:04 +0800

    comment to-fix code (/StreamSave2SQL.scala)

--- src/main/scala/StreamBasics/StreamSave2SQL.scala
+++ src/main/scala/StreamBasics/StreamSave2SQL.scala
@@ -1,49 +1,53 @@
-package SparkStreamingBasics
+// package SparkStreamingBasics
 
-import org.apache.log4j.{Level, Logger}
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.streaming.{Seconds, StreamingContext}
-import org.apache.spark.streaming.twitter.TwitterUtils
-import util.Twitter
+// import org.apache.log4j.{Level, Logger}
+// import org.apache.spark.streaming.{Seconds, StreamingContext}
+// import org.apache.spark.sql.{Row, SaveMode, SparkSession}
+// import org.apache.spark.streaming.twitter.TwitterUtils
+// import util.Twitter
 
-object StreamSave2SQL {
-    def main(args: Array[String]): Unit = {
+// object StreamSave2SQL {
+//     def main(args: Array[String]): Unit = {
 
-        Twitter.initialize()
+//         Twitter.initialize()
 
-        val ssc = new StreamingContext("local[*]", "SqlOperations", Seconds(10))
+//         // init spark stream
+//         val ssc = new StreamingContext("local[*]", "SqlOperations", Seconds(10))
 
-        Logger.getRootLogger.setLevel(Level.ERROR)
+//         // init spark DB : warehouseLocation points to the default location for managed databases and tables
+//         val warehouseLocation = new File("spark-warehouse").getAbsolutePath
 
-        //val stream = RedditUtils.createPageStream(Reddit.auth, List("sports"), ssc, pollingPeriodInSeconds=10)
+//         Logger.getRootLogger.setLevel(Level.ERROR)
 
-        val stream = TwitterUtils.createStream(ssc, None)
+//         //val stream = RedditUtils.createPageStream(Reddit.auth, List("sports"), ssc, pollingPeriodInSeconds=10)
 
-        stream
-            .map(p => (p.getId , 
-                       p.getUser.getName, 
-                       (if (p.getPlace == null) "" else p.getPlace.getName), 
-                       p.getInReplyToScreenName,
-                       p.getCreatedAt.getTime, 
-                       p.getText.length, 
-                       p.getHashtagEntities.map(_.getText).headOption.getOrElse(""))
-                       )
-            .foreachRDD { rdd =>
-                val spark = SparkSession.builder.config(rdd.sparkContext.getConf).getOrCreate()
-                import spark.implicits._
+//         val stream = TwitterUtils.createStream(ssc, None)
 
-                // Convert RDD to DataFrame
-                val df = rdd.toDF("id","username","place","reply_screen_name","time","text_length", "hasg_tag")
+//         stream
+//             .map(p => (p.getId , 
+//                        p.getUser.getName, 
+//                        (if (p.getPlace == null) "" else p.getPlace.getName), 
+//                        p.getInReplyToScreenName,
+//                        p.getCreatedAt.getTime, 
+//                        p.getText.length, 
+//                        p.getHashtagEntities.map(_.getText).headOption.getOrElse(""))
+//                        )
+//             .foreachRDD { rdd =>
+//                 val spark = SparkSession.builder.config(rdd.sparkContext.getConf).getOrCreate()
+//                 import spark.implicits._
 
-                // Create a temporary view
-                df.createOrReplaceTempView("twitter")
+//                 // Convert RDD to DataFrame
+//                 val df = rdd.toDF("id","username","place","reply_screen_name","time","text_length", "hasg_tag")
 
-                spark.sql("select * from twitter").show()
+//                 // Create a temporary view
+//                 df.createOrReplaceTempView("twitter")
 
-                //spark.sql("select text_length, count(*) from twitter group by 1 order by 2 desc").show()
-            }
+//                 spark.sql("select * from twitter").show()
 
-        ssc.start
-        ssc.awaitTermination()
-    }
-}
+//                 //spark.sql("select text_length, count(*) from twitter group by 1 order by 2 desc").show()
+//             }
+
+//         ssc.start
+//         ssc.awaitTermination()
+//     }
+// }
