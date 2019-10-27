c40cd8c636100959fecdfa32e576186482c53219
yennanliu <f339339@gmail.com>
2019-10-27 16:41:21 +0800

    add more game metric rdd (SparkProcessGameRDD.scala)

--- src/main/scala/sparkhelloworld/SparkProcessGameRDD.scala
+++ src/main/scala/sparkhelloworld/SparkProcessGameRDD.scala
@@ -8,13 +8,15 @@ object SparkProcessGameRDD{
 
     def main(args: Array[String]){
 
-        val conf = new SparkConf().setMaster("local[*]").setAppName("SparkRDDHelloworld")
+        val conf = new SparkConf()
+                       .setMaster("local[*]")
+                       .setAppName("SparkRDDHelloworld")
         var sc = new SparkContext(conf)
         val spark = SparkSession
-                  .builder
-                  .appName("Spark sql demo")
-                  .config("spark.master", "local")
-                  .getOrCreate()
+                    .builder
+                    .appName("Spark sql demo")
+                    .config("spark.master", "local")
+                    .getOrCreate()
 
         import spark.implicits._
         sc.setLogLevel("ERROR")
@@ -26,8 +28,15 @@ object SparkProcessGameRDD{
         df.printSchema()
         var dfRDD = df.rdd
         dfRDD.take(10).foreach(println)
-        var game_id = dfRDD.map(x => x(1))
-        game_id.take(30).foreach(println)
+        var battle_id = dfRDD.map(x => x(1)).filter(x => x != null) 
+        battle_id.take(30).foreach(println)
+        var event_timestamp = dfRDD.map(x => x(4)).filter(x => x != null)
+        event_timestamp.take(30).foreach(println)
+        //var platform_count = dfRDD.map(x => x(8)).filter(x => x != null).map(x => (x(8),1))
+        var platform_count = dfRDD.map(x => (x(8),1))
+        platform_count.take(30).foreach(println)
+
+
         // val lines = sc.textFile(filePath)
         // val words = lines.flatMap(line => line.split(" "))
         // val counts = words.map(word => (word, 1)).reduceByKey { case (x, y) => x + y }
