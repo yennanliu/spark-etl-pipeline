c083bc2bb279296ea9a02782f66cef15f4ac29c3
yennanliu <f339339@gmail.com>
2019-11-03 09:55:50 +0800

    update run_all_process.sh

--- run_all_process.sh
+++ run_all_process.sh
@@ -12,4 +12,4 @@ type=bind,\
 source="$(pwd)"/.,\
 target=/spark-etl-pipeline \
 -i -t spark_env \
-/bin/bash  -c "cd ../spark-etl-pipeline && sbt clean compile && sbt run"
+/bin/bash  -c "cd ../spark-etl-pipeline && sbt clean compile && sbt assembly && spark-submit /spark-etl-pipeline/target/scala-2.11/spark-etl-pipeline-assembly-1.0.jar"
