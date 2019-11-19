9b55befc77d17a549ac65039a359354307f64d76
yennanliu <f339339@gmail.com>
2019-11-19 22:45:04 +0800

    add get_AWS_ACCESS_KEY_ID, get_AWS_SECRET_ACCESS_KEY func to S3Loader.scala

--- src/main/scala/util/S3Loader.scala
+++ src/main/scala/util/S3Loader.scala
@@ -6,7 +6,17 @@ import com.typesafe.config.ConfigFactory
 object S3Loader {
     def initialize(): Unit = {
         val config = ConfigFactory.parseFile(new File("config/s3.config"))
-        System.setProperty("accessKeyId", config.getString("AWS_ACCESS_KEY_ID"))
-        System.setProperty("secretAccessKey", config.getString("AWS_SECRET_ACCESS_KEY"))
+        System.setProperty("AWS_ACCESS_KEY_ID", config.getString("AWS_ACCESS_KEY_ID"))
+        System.setProperty("AWS_SECRET_ACCESS_KEY", config.getString("AWS_SECRET_ACCESS_KEY"))
+    }
+
+    def get_AWS_ACCESS_KEY_ID(): String = {
+        val config = ConfigFactory.parseFile(new File("config/s3.config"))
+        ConfigFactory.parseFile(new File("config/s3.config")).getString("AWS_ACCESS_KEY_ID")
+    }
+
+    def get_AWS_SECRET_ACCESS_KEY(): String = {
+        val config = ConfigFactory.parseFile(new File("config/s3.config"))
+        ConfigFactory.parseFile(new File("config/s3.config")).getString("AWS_SECRET_ACCESS_KEY")
     }
 }
